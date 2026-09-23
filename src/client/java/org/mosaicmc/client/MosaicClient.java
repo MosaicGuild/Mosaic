b26b7d1f58b8a46755982e3d09bca7c75e90d797
Sutanu Krishna Majumder <majumdernupur888@gmail.com>
2026-09-23 16:10:30 +0530

    Added auto-enabler (TODO: remove this)

--- src/client/java/org/mosaicmc/client/MosaicClient.java
+++ src/client/java/org/mosaicmc/client/MosaicClient.java
@@ -4,6 +4,7 @@ import org.mosaicmc.client.key.MosaicKeyEventHandler;
 import org.mosaicmc.client.key.MosaicKeys;
 import net.fabricmc.api.ClientModInitializer;
 import org.mosaicmc.Mosaic;
+import org.mosaicmc.extension.Extension;
 import org.mosaicmc.extension.ExtensionManager;
 
 public class MosaicClient implements ClientModInitializer {
@@ -14,6 +15,24 @@ public class MosaicClient implements ClientModInitializer {
 				ClientExtensionScheduler.INSTANCE.getClass().getSimpleName());
 		ManagedClientTick.installOnce();
 		Mosaic.LOGGER.info("[Mosaic] managed client tick installed");
+
+		// This code auto enables extension
+		// TODO : remove this in favour of gui based enable system
+		int enabled = 0;
+		for (Extension extension : ExtensionManager.getExtensions()) {
+			String id;
+			try {
+				id = extension.getMetadata().getId();
+			} catch (Exception e) {
+				Mosaic.LOGGER.error("[Mosaic] skipping auto-enable, broken metadata", e);
+				continue;
+			}
+			ExtensionManager.enable(id);
+			enabled++;
+		}
+		Mosaic.LOGGER.info("[Mosaic] auto-enabled {} extension(s) (no toggle UI yet)", enabled);
+
+
 		MosaicKeys.init();
 		MosaicKeyEventHandler.init();
 	}
